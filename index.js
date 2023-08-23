15fb5a08ce9c40ec8e450237e6dd3a46a47857d4
moon139565 <anika01839@gmail.com>
2023-08-23 11:31:27 +0600

    okay

--- index.js
+++ index.js
@@ -57,3 +57,63 @@ circles.forEach(element=>{
         pointsMarked[i].classList.add('marked')
     }
 })
+
+
+// mix it up protflio section ---------
+
+var mixer = mixitup('.protfolio-gallery');
+
+// active menu ----------------------
+
+let menuli = document.querySelectorAll("header ul li a")
+let section = document.querySelectorAll("section")
+
+function actveMenu(){
+    let len = section.length;
+    while(--len && window.scrollY + 97 < section[len].offsetTop){}
+    menuli.forEach(sec => sec.classList.remove("active"));
+    menuli[len].classList.add("active")
+}
+actveMenu()
+window.addEventListener("scroll", actveMenu)
+
+// sticky nav--------------
+const header = document.querySelector("header")
+window.addEventListener("scroll",function(){
+    header.classList.toggle("sticky", window.scrollY > 50)
+})
+
+// toggle icone ---------------------
+let menuIcon = document.querySelector("#menu-icon")
+let navlist = document.querySelector(".navlist")
+
+menuIcon.onclick =()=>{
+    menuIcon.classList.toggle("bx-x")
+    navlist.classList.toggle("open")
+}
+window.onscroll =()=>{
+    menuIcon.classList.remove("bx-x")
+    navlist.classList.remove("open")
+}
+
+
+// scroll animation -----------
+
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach((enter)=>{
+        if(enter.isIntersecting){
+            enter.target.classList.add("show-items")
+        }else{
+            enter.target.classList.remove("show-items")
+        }
+    })
+})
+
+const scrollScal = document.querySelectorAll(".scroll-scale")
+scrollScal.forEach((el)=> observer.observe(el))
+
+const scrollBottom = document.querySelectorAll(".scroll-bottom")
+scrollBottom.forEach((el)=> observer.observe(el))
+
+const scrollTop = document.querySelectorAll(".scroll-top")
+scrollTop.forEach((el)=> observer.observe(el))
