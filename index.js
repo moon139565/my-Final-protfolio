3188fc3db65e144ade0cdcc548ad71a12c5c9794
moon139565 <anika01839@gmail.com>
2023-08-24 20:20:33 +0600

    new

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 let words = document.querySelectorAll(".word")
-words.forEach((word)=>{
+words.forEach((word) => {
     let letters = word.textContent.split("")
-    word.textContent="";
-    letters.forEach((letter)=>{
+    word.textContent = "";
+    letters.forEach((letter) => {
         let span = document.createElement("span")
         span.textContent = letter;
         span.className = "letter"
@@ -11,49 +11,49 @@ words.forEach((word)=>{
 });
 
 let currentWordIndex = 0;
-let maxWordIndex = words.length -1;
-words[currentWordIndex].style.opacity ="1";
+let maxWordIndex = words.length - 1;
+words[currentWordIndex].style.opacity = "1";
 
-let changeText = ()=>{
+let changeText = () => {
     let currentWord = words[currentWordIndex];
     let nextWord = currentWordIndex === maxWordIndex ? words[0] : words[currentWordIndex + 1];
 
-    Array.from(currentWord.children).forEach((letter,i)=>{
+    Array.from(currentWord.children).forEach((letter, i) => {
         setTimeout(() => {
             letter.className = "letter out";
-        },i* 80);
+        }, i * 80);
     });
 
-    nextWord.style.opacity="1"
-    Array.from(nextWord.children).forEach((letter,i)=>{
+    nextWord.style.opacity = "1"
+    Array.from(nextWord.children).forEach((letter, i) => {
         letter.className = "letter behind"
-        setTimeout(()=>{
-            letter.className="letter in"
-        },340 + i * 80)
+        setTimeout(() => {
+            letter.className = "letter in"
+        }, 340 + i * 80)
     })
     currentWordIndex = currentWordIndex === maxWordIndex ? 0 : currentWordIndex + 1;
 }
 
 changeText()
-setInterval(changeText,3000)
+setInterval(changeText, 3000)
 
 
 // circle skills -----------
 
 const circles = document.querySelectorAll('.circle');
-circles.forEach(element=>{
+circles.forEach(element => {
     let dots = element.getAttribute("data-dots");
     let marked = element.getAttribute("data-percent");
-    let percent = Math.floor(dots*marked/100);
-    let points =""
+    let percent = Math.floor(dots * marked / 100);
+    let points = ""
     let rotate = 360 / dots;
 
-    for(let i = 0; i < dots; i++){
+    for (let i = 0; i < dots; i++) {
         points += `<div class="points" style="--i:${i}; --rot:${rotate}deg"></div>`
     }
     element.innerHTML = points;
     const pointsMarked = element.querySelectorAll('.points')
-    for(let i = 0 ; i < percent; i++){
+    for (let i = 0; i < percent; i++) {
         pointsMarked[i].classList.add('marked')
     }
 })
@@ -68,9 +68,9 @@ var mixer = mixitup('.protfolio-gallery');
 let menuli = document.querySelectorAll("header ul li a")
 let section = document.querySelectorAll("section")
 
-function actveMenu(){
+function actveMenu() {
     let len = section.length;
-    while(--len && window.scrollY + 97 < section[len].offsetTop){}
+    while (--len && window.scrollY + 97 < section[len].offsetTop) { }
     menuli.forEach(sec => sec.classList.remove("active"));
     menuli[len].classList.add("active")
 }
@@ -79,7 +79,7 @@ window.addEventListener("scroll", actveMenu)
 
 // sticky nav--------------
 const header = document.querySelector("header")
-window.addEventListener("scroll",function(){
+window.addEventListener("scroll", function () {
     header.classList.toggle("sticky", window.scrollY > 50)
 })
 
@@ -87,11 +87,11 @@ window.addEventListener("scroll",function(){
 let menuIcon = document.querySelector("#menu-icon")
 let navlist = document.querySelector(".navlist")
 
-menuIcon.onclick =()=>{
+menuIcon.onclick = () => {
     menuIcon.classList.toggle("bx-x")
     navlist.classList.toggle("open")
 }
-window.onscroll =()=>{
+window.onscroll = () => {
     menuIcon.classList.remove("bx-x")
     navlist.classList.remove("open")
 }
@@ -99,21 +99,65 @@ window.onscroll =()=>{
 
 // scroll animation -----------
 
-const observer = new IntersectionObserver((entries)=>{
-    entries.forEach((enter)=>{
-        if(enter.isIntersecting){
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((enter) => {
+        if (enter.isIntersecting) {
             enter.target.classList.add("show-items")
-        }else{
+        } else {
             enter.target.classList.remove("show-items")
         }
     })
 })
 
 const scrollScal = document.querySelectorAll(".scroll-scale")
-scrollScal.forEach((el)=> observer.observe(el))
+scrollScal.forEach((el) => observer.observe(el))
 
 const scrollBottom = document.querySelectorAll(".scroll-bottom")
-scrollBottom.forEach((el)=> observer.observe(el))
+scrollBottom.forEach((el) => observer.observe(el))
 
 const scrollTop = document.querySelectorAll(".scroll-top")
-scrollTop.forEach((el)=> observer.observe(el))
+scrollTop.forEach((el) => observer.observe(el))
+
+
+
+// contact from ------------------
+
+const submitButton = document.getElementById("submitButton")
+
+
+submitButton.addEventListener("click", function () {
+    const contactName = document.getElementById("contactName")
+    const contactemail = document.getElementById("contactemail")
+    const contactmessage = document.getElementById("contactmessage")
+    const suceesMail = document.querySelector(".sucees-mail");
+    const errorMail = document.querySelector(".error-mail");
+
+    const namevalue = contactName.value;
+    const emailvalue = contactemail.value;
+    const messagevalue = contactmessage.value;
+
+    const templateParams = {
+        namevalue: namevalue,
+        emailvalue: emailvalue,
+        messagevalue: messagevalue
+    };
+
+    if (namevalue && emailvalue && messagevalue) {
+        console.log(namevalue, emailvalue, messagevalue);
+
+        emailjs.send("service_17spcy8", "template_hj3nieo", templateParams, "vdWpjsEJJbBs5a582");
+        suceesMail.classList.add("sucees-show")
+        setTimeout(function () {
+            suceesMail.classList.remove("sucees-show")
+
+        }, 4000)
+        contactName.value = "";
+        contactemail.value = "";
+        contactmessage.value = "";
+    } else {
+        errorMail.classList.add("show-error")
+        setTimeout(() => {
+            errorMail.classList.remove("show-error")
+        }, 4000);
+    }
+})
